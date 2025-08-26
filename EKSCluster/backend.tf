59716144bf859ef99b13a3448fe768964e0ab4c2
AurelienKumarathas <akumarathas27@gmail.com>
2025-08-26 13:34:53 +0100

    Update backend.tf

--- EKSCluster/backend.tf
+++ EKSCluster/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "XX" # CHANGE WITH YOUR S3 BUCKET
+    bucket = "terraform-august-project25" 
     key    = "K8inEKS/terraform.tfstate"
     region = "eu-west-2"
   }
